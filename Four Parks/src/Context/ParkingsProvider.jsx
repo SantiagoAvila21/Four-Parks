1d8214e07fe9ecc5615b41fd7544954c4e05286e
Santiago Avila <santiavilag2015@gmail.com>
2024-05-11 02:13:21 -0500

    Se agrega las funcionalidades respectivas de los parqueaderos, conectados con la Base de datos

--- Four Parks/src/Context/ParkingsProvider.jsx
+++ Four Parks/src/Context/ParkingsProvider.jsx
@@ -1,14 +1,37 @@
-import { useContext, createContext } from "react"
+import { useContext, createContext, useState, useEffect } from "react"
+import axios from 'axios'
 
 const ParkingContext = createContext();
 
 /* eslint-disable react/prop-types */
-export const ParkingProvider = ({ children }) => {
+const ParkingProvider = ({ children }) => {
+    const [parqueaderos, setParqueaderos] = useState([]);
+
+    useEffect(() => {
+        const fetchParqueaderos = (async () => {
+            try {
+                const response = await axios.get(`${import.meta.env.VITE_FLASK_SERVER_URL}/api/get_parqueaderos`);
+          
+                console.log('Respuesta del servidor:', response);
+                if(response.status == 200){
+                    setParqueaderos(response.data);
+                    return;
+                }
+              } catch (error) {
+                console.error("Error de la solicitud: ", error.response.data.error)
+            }
+        })();
+    }, []);
+
     return (
-        <ParkingContext.Provider>
+        <ParkingContext.Provider value={{ parqueaderos }} >
             {children}
         </ParkingContext.Provider>
     );
 }
 
-export const useParkingContext = () => useContext(ParkingContext); 
+export default ParkingProvider;
+
+export const useParking = () => {
+    return useContext(ParkingContext); 
+}
--- Four Parks/src/components/MyMap.jsx
+++ Four Parks/src/components/MyMap.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import MarkerInfo from "./MarkerInfo";
 import './styles/Markerinfo.css'
 import { useMarkerContext } from "../Context/MarkerProvider";
+import { useParking } from "../Context/ParkingsProvider";
 
 function mapTiler(x, y, z, dpr) {
   return `https://api.maptiler.com/maps/${import.meta.env.VITE_MAP_ID}/256/${z}/${x}/${y}${
@@ -11,9 +12,12 @@ function mapTiler(x, y, z, dpr) {
 }
 
 const MyMap = () => {
+  const parking = useParking();
+  
   const [currentLocation, setCurrentLocation] = useState([0, 0]);
   const [showOverlay, setShowOverlay] = useState(false);
   const [overlayPosition, setOverlayPosition] = useState([0, 0]);
+  const [selectedParking, setSelectedParking] = useState({});
 
   const { selectedMarkerPosition } = useMarkerContext();
 
@@ -23,7 +27,13 @@ const MyMap = () => {
     });
   }, []);
 
-  const handleMarkerClick = (position) => {
+  const handleMarkerClick = (position, parqueaderoInfo) => {
+    setSelectedParking({
+      nombreParqueadero: parqueaderoInfo[2],
+      direccion: parqueaderoInfo[3],
+      tipo: parqueaderoInfo[1],
+      espacioActual: parqueaderoInfo[5]
+    });
     setOverlayPosition(position);
     setShowOverlay(true);
   };
@@ -35,41 +45,19 @@ const MyMap = () => {
       center = {selectedMarkerPosition || currentLocation}
       defaultZoom = {17}
     >
-      <Marker
-        onClick={() => handleMarkerClick([4.62958, -74.065738])}
-        width={40}
-        color={"#F96E08"}
-        anchor={[4.62958, -74.065738]}
-      />
-      <Marker
-        onClick={() => handleMarkerClick([4.60971, -74.08175])}
-        width={40}
-        color={"#F96E08"}
-        anchor={[4.60971, -74.08175]} 
-      />
-      <Marker
-        onClick={() => handleMarkerClick([4.648283, -74.077133])}
-        width={40}
-        color={"#F96E08"}
-        anchor={[4.648283, -74.077133]} 
-      />
-      <Marker
-        onClick={() => handleMarkerClick([4.63615, -74.06856])}
-        width={40}
-        color={"#F96E08"}
-        anchor={[4.63615, -74.06856]} 
-      />
-      <Marker
-        onClick={() => handleMarkerClick([4.60971, -74.06606])}
-        width={40}
-        color={"#F96E08"}
-        anchor={[4.60971, -74.06606]} 
-      />
-
+      {parking.parqueaderos.map((parqueadero) => {
+        return <Marker
+          key={parqueadero[0]}
+          onClick={() => handleMarkerClick([parqueadero[8], parqueadero[7]], parqueadero)}
+          width={40}
+          color={"#F96E08"}
+          anchor={[parqueadero[8], parqueadero[7]]}
+        />
+      })}
       {showOverlay && (
         <Overlay anchor={overlayPosition} offset={[100, 0]}>
           <div onClick={() => setShowOverlay(false)} className="overlay">
-            <MarkerInfo />
+            <MarkerInfo info={selectedParking}/> 
           </div>
         </Overlay>
       )}
