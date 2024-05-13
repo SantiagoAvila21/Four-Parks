6bc6ffcdbb5a7ce938c9aa563067def04b497dfb
Santiago Avila <santiavilag2015@gmail.com>
2024-05-13 01:25:58 -0500

    Se realiza la correccion de la pagina Login, para verificar tanto el codigo de verificacion, como la primera vez que se inicia sesion en la base de datos

--- Four Parks/src/Context/AuthProvider.jsx
+++ Four Parks/src/Context/AuthProvider.jsx
@@ -10,44 +10,85 @@ const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [state, setState] = useState("");
     const navigate = useNavigate();
-    const { updateNotification } = useNotification();
+    const { updateNotification, closeNoti } = useNotification();
 
+    // Comprobar si hay un usuario almacenado en el localStorage al iniciar la aplicación
     useEffect(() => {
-        // Comprobar si hay un usuario almacenado en el localStorage al iniciar la aplicación
         const userFromLocalStorage = localStorage.getItem("userLogged");
         if (userFromLocalStorage) {
             setUser(userFromLocalStorage); // Convertir la cadena JSON almacenada en el localStorage de nuevo a un objeto
         }
     }, []);
 
+    // Función para realizar la acción de inicio de sesión
     const loginAction = async (data, cb) => {
+        // Notificación de carga mientras se procesa la solicitud
+        updateNotification({type: 'loading', message: 'Cargando...'});
         try {
+            // Realizar la solicitud de inicio de sesión al servidor
             const response = await axios.post(`${import.meta.env.VITE_FLASK_SERVER_URL}/login`, {
-              correoelectronico: data.email,
-              contrasenia: data.password
+                correoelectronico: data.email,
+                contrasenia: data.password
             });
-      
+        
             console.log('Respuesta del servidor:', response);
+            // Si la solicitud es exitosa
             if(response.status == 200){
-                setUser(response.data.usuario);
-                localStorage.setItem("userLogged", response.data.usuario);
+                // Cerrar la notificación de carga
+                closeNoti();
+                // Llamar a la función de callback proporcionada
                 cb();
                 return;
             }
-          } catch (error) {
+        } catch (error) {
+            // Cerrar la notificación de carga en caso de error
+            closeNoti();
             console.error("Error de la solicitud: ", error.response.data.error)
+            // Actualizar la notificación con el error recibido
+            updateNotification({ type: 'error', message: error.response.data.error });
+        }
+        // Cerrar la notificación después de completar la acción
+        closeNoti();
+    }
+
+    // Función para verificar el código de verificación
+    const verifyCode = async (code, email) => {
+        // Notificación de carga mientras se procesa la solicitud
+        updateNotification({type: 'loading', message: 'Cargando...'});
+        try {
+            // Realizar la solicitud de verificación al servidor
+            const response = await axios.post(`${import.meta.env.VITE_FLASK_SERVER_URL}/verify`, {
+                correoelectronico: email,
+                codigo: code
+            });
+            // Si la verificación es exitosa
+            if(response.status == 200){
+                // Cerrar la notificación de carga
+                closeNoti();
+                // Establecer el usuario y almacenarlo en el almacenamiento local
+                setUser(response.data.usuario);
+                localStorage.setItem("userLogged", response.data.usuario);
+                // Cambiar el estado de la aplicación a "logged"
+                setState('logged');
+                // Redirigir a la página de inicio
+                navigate("/");
+                return;
+            }
+        } catch (error) {
+            // Cerrar la notificación de carga en caso de error
+            closeNoti();
+            console.error("Error de la solicitud: ", error.response.data.error)
+            // Actualizar la notificación con el error recibido
             updateNotification({ type: 'error', message: error.response.data.error });
         }
     }
 
-    const verifyCode = (code) => {
-        console.log(code);
-        setState('logged');
-        navigate("/");
-    }
-
+    // Función para realizar la acción de registro de usuario
     const registerAction = async(data) => {
+        // Notificación de carga mientras se procesa la solicitud
+        updateNotification({type: 'loading', message: 'Cargando...'});
         try{
+            // Realizar la solicitud de registro al servidor
             const responseRegister = await axios.post(`${import.meta.env.VITE_FLASK_SERVER_URL}/register`, {
                 idtipousuario: "3",
                 idtipodocumento: data.tipoDoc,
@@ -56,19 +97,30 @@ const AuthProvider = ({ children }) => {
                 puntosacumulados: 0,
                 correoelectronico: data.email
             });
+            // Si el registro es exitoso
             if(responseRegister.status == 201){
+                // Cerrar la notificación de carga
+                closeNoti();
+                // Cambiar el estado de la aplicación a "registered"
                 setState('registered');
+                // Redirigir a la página de inicio de sesión
                 navigate("/login");
             }
         }catch(error){
+            // Cerrar la notificación de carga en caso de error
+            closeNoti();
             console.log("Error de la solicitud: ", error.response.data.error)
+            // Actualizar la notificación con el error recibido
             updateNotification({ type: 'error', message: error.response.data.error });
         }
     }
 
+    // Función para cerrar sesión del usuario
     const logOut = () => {
+        // Eliminar la información del usuario del almacenamiento local
         setUser(null);
         localStorage.removeItem("userLogged");
+        // Redirigir a la página de inicio de sesión
         navigate("/login");
     };
 
--- Four Parks/src/Hooks/useNotification.jsx
+++ Four Parks/src/Hooks/useNotification.jsx
@@ -23,7 +23,7 @@ const useNotification = (initialNotification) => {
     const { type, message, position, autoClose, hideProgressBar, closeOnClick, pauseOnHover, draggable, progress, theme } = notification;
 
     useEffect(() => {
-        if (type === 'error' || type === 'success' || type === 'info') {
+        if (type === 'error' || type === 'success' || type === 'info' || type === 'loading' ) {
             toast[type](message, {
                 position,
                 autoClose,
@@ -45,11 +45,11 @@ const useNotification = (initialNotification) => {
     };
 
     // Función para manejar la promesa y mostrar las notificaciones Toastify
-    const handlePromise = async (promise, options) => {
-        toast.promise(promise, options);
+    const closeNoti = () => {
+        toast.dismiss();
     };
 
-    return { updateNotification, handlePromise };
+    return { updateNotification, closeNoti };
 };
 
 export default useNotification;
