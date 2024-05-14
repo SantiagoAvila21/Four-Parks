f40bf70685a28d15cb6c61d71d8f09e14956ca96
Santiago Avila <santiavilag2015@gmail.com>
2024-05-13 22:57:08 -0500

    Se soluciona el error del Espacio en el first_login

--- FourParksBackEnd/app.py
+++ FourParksBackEnd/app.py
@@ -153,7 +153,7 @@ def login():
         # Revisamos si la contraseña hasheada es la misma que esta en la Base de Datos
         if password_hash == user_password[0]:
             # Actualizar el campo first_login en la base de datos
-            cur.execute("UPDATE usuario SET first_ login = %s WHERE correoelectronico = %s", (False, email))
+            cur.execute("UPDATE usuario SET first_login = %s WHERE correoelectronico = %s", (False, email))
             conn.commit()
 
             # Generacion del codigo de verificacion
