84cc730ef8de1d75733c409d631d0e71d7001e75
Santiago Avila <santiavilag2015@gmail.com>
2024-05-30 01:47:47 -0500

    Eliminado React awesome reveal

--- Four Parks/src/Pages/Landing.jsx
+++ Four Parks/src/Pages/Landing.jsx
@@ -5,7 +5,7 @@ import { About } from "../components/LandingComponents/acercaDe";
 import { Services } from "../components/LandingComponents/ventajas";
 import { Testimonials } from "../components/LandingComponents/experiencias";
 import JsonData from "../data/data.json";
-import { Fade, Slide, JackInTheBox } from "react-awesome-reveal";
+//import { Fade, Slide, JackInTheBox } from "react-awesome-reveal";
 
 const Landing = () => {
   const landingPageData = JsonData;
@@ -13,17 +13,11 @@ const Landing = () => {
   return (
     <div>
         <Navigation />
-        <Fade triggerOnce>
-          <Header data={landingPageData.Header} />
-        </Fade>
+        <Header data={landingPageData.Header} />
         <Features data={landingPageData.Features} /> 
-        <Slide triggerOnce>
-          <About data={landingPageData.About} />
-        </Slide>
+        <About data={landingPageData.About} />
         <Services data={landingPageData.Services} />
-        <JackInTheBox triggerOnce>
-          <Testimonials data={landingPageData.Testimonials} />
-        </JackInTheBox>
+        <Testimonials data={landingPageData.Testimonials} />
     </div>
   );
 };
